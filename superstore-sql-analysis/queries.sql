7676a2c025c7000a3f88fd7ed211acf24b6f8990
HaarisMalik-hub <muhammadhaarismalik@gmail.com>
2025-05-14 18:05:34 -0400

    Update queries.sql

--- superstore-sql-analysis/queries.sql
+++ superstore-sql-analysis/queries.sql
@@ -1,27 +1,32 @@
--- Total Sales by Region
-SELECT Region, SUM(Sales) AS Total_Sales
+-- 💡 a) Total Sales by Region
+SELECT Region, ROUND(SUM(Sales), 2) AS Total_Sales
 FROM sales
 GROUP BY Region
 ORDER BY Total_Sales DESC;
 
--- Top 5 Products by Profit
-SELECT Product_Name, SUM(Profit) AS Total_Profit
+-- 💡 b) Profit by Product Category
+SELECT Category, ROUND(SUM(Profit), 2) AS Total_Profit
 FROM sales
-GROUP BY Product_Name
-ORDER BY Total_Profit DESC
-LIMIT 5;
+GROUP BY Category
+ORDER BY Total_Profit DESC;
 
--- Top 10 Products by Sales
+-- 💡 c) Top 10 Products by Sales
 SELECT "Product Name", ROUND(SUM(Sales), 2) AS Total_Sales
 FROM sales
 GROUP BY "Product Name"
 ORDER BY Total_Sales DESC
 LIMIT 10;
 
--- Products with Negative Profit
+-- 💡 d) Products with Negative Profit
 SELECT "Product Name", ROUND(SUM(Profit), 2) AS Total_Profit
 FROM sales
 GROUP BY "Product Name"
 HAVING Total_Profit < 0
 ORDER BY Total_Profit ASC
 LIMIT 10;
+
+-- 💡 e) Customer Segments with Highest Profit
+SELECT Segment, ROUND(SUM(Profit), 2) AS Total_Profit
+FROM sales
+GROUP BY Segment
+ORDER BY Total_Profit DESC;
